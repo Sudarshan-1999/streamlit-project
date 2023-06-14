ec3df1a4c57252623562914550c165c4a31e1499
Sudarshan Ramdas Damahe <sudarshan.damahe@rampgroup.com>
2023-06-14 12:15:02 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+# streamlit_app.py
+
+import streamlit as st
+import pymongo
+
+# Initialize connection.
+# Uses st.cache_resource to only run once.
+@st.cache_resource
+def init_connection():
+    return pymongo.MongoClient(**st.secrets.mongo)
+    # return pymongo.MongoClient(f"mongodb+srv://{st.secrets.username}:{st.secrets.password}@{st.secrets.host}")
+
+client = init_connection()
+
+# Pull data from the collection.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def get_data():
+    db = client.test
+    items = db.mycollection.find()
+    items = list(items)  # make hashable for st.cache_data
+    return items
+
+items = get_data()
+
+# Print results.
+for item in items:
+    st.write(f"{item['name']} has a :{item['pet']}:")
