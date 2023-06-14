b8c06c872207b1cfe8697762f99c5dd191ab9201
Sudarshan Ramdas Damahe <sudarshan.damahe@rampgroup.com>
2023-06-14 12:22:31 +0530

    code update creds

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pymongo
 # Uses st.cache_resource to only run once.
 @st.cache_resource
 def init_connection():
-    return pymongo.MongoClient(**st.secrets.mongo)
+    return pymongo.MongoClient(**st.secrets["mongo"])
     # return pymongo.MongoClient(f"mongodb+srv://{st.secrets.username}:{st.secrets.password}@{st.secrets.host}")
 
 client = init_connection()
